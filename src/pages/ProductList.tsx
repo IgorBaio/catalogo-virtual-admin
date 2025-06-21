67f5fee678146e841f44e72abfa7458d5be54094
IgorBaio <37658514+IgorBaio@users.noreply.github.com>
2025-06-21 11:37:21 -0300

    Atualizar URLs da API

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -23,7 +23,7 @@ export default function ProductList() {
   useEffect(() => {
     async function load() {
       try {
-        const data = await getProducts()
+        const data = await getProducts('all')
         setProducts(data)
       } catch (err) {
         console.error(err)
