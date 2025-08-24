f550e42cedf45e21572e11a91570a2a72a72c413
igorbaio <igorbaiosoares@gmail.com>
2025-08-24 16:23:44 -0300

    update the css to the logout button

--- src/components/DrawerMenu.tsx
+++ src/components/DrawerMenu.tsx
@@ -1,15 +1,15 @@
-import { Menu } from 'lucide-react'
-import { Link, useNavigate } from 'react-router-dom'
-import { Sheet, SheetTrigger, SheetContent } from '@/components/ui/sheet'
-import { Button } from '@/components/ui/button'
+import { Menu } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
 
 export default function DrawerMenu() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   function handleLogout() {
-    localStorage.removeItem('logged')
-    localStorage.removeItem('userData')
-    navigate('/login')
+    localStorage.removeItem("logged");
+    localStorage.removeItem("userData");
+    navigate("/login");
   }
 
   return (
@@ -22,13 +22,28 @@ export default function DrawerMenu() {
           <Menu />
         </Button>
       </SheetTrigger>
-      <SheetContent side="left" className="w-64">
+      <SheetContent
+        side="left"
+        className="w-64"
+        style={{
+          display: "flex",
+          alignItems: "flex-start",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
         <nav className="mt-8 flex flex-col gap-4">
           <Link to="/products">Cadastro de Produtos</Link>
           <Link to="/list">Lista de Produtos</Link>
-          <Button variant="destructive" onClick={handleLogout}>Sair</Button>
         </nav>
+
+        <div className="mb-0 p-4 text-center text-sm text-gray-500">
+          &copy; {new Date().getFullYear()} BaioSystems
+          <Button variant="destructive" className="mt-2"  onClick={handleLogout}>
+            Sair
+          </Button>
+        </div>
       </SheetContent>
     </Sheet>
-  )
+  );
 }
