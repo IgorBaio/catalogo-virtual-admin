b3b8872ba0cdcdccceb2dba29ef49d80307ba054
IgorBaio <IgorBaio@users.noreply.github.com>
2025-11-17 19:56:45 +0000

    deploy: ae778fab757b9274a64566c9e287c9a67e3eda9b

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/catalogo-virtual-admin/sw.js', { scope: '/catalogo-virtual-admin/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
